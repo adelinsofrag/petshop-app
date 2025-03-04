684c258b8d73def366d80f13bcd0c1a0eb30ec6e
adelinsofrag <adelin.sofrag@gmail.com>
2025-03-04 20:37:45 +0200

    implements fetch location for vet details

--- scripts.js
+++ scripts.js
@@ -9,11 +9,29 @@ const petShop = {
     updateSelectOptions();
   },
 
-  addVet(name, specialization) {
-    this.vets.push({ name, specialization });
+  addVet(name, specialization, location, callback) {
+    validateLocation(location, (response) => {
+      if (response.validatedLocation) {
+        this.vets.push({
+          name,
+          specialization,
+          location: response.validatedLocation,
+        });
 
-    updateVetsTable();
-    updateSelectOptions();
+        updateVetsTable();
+        updateSelectOptions();
+
+        callback({
+          success: true,
+          validatedLocation: response.validatedLocation,
+        });
+      } else {
+        callback({
+          success: false,
+          message: response.message,
+        });
+      }
+    });
   },
 
   assignVetToPet(vetName, petName) {
@@ -47,9 +65,32 @@ function addPet() {
 function addVet() {
   const name = document.getElementById("vetName").value;
   const specialization = document.getElementById("vetSpecialization").value;
+  const location = document.getElementById("vetLocation").value;
 
-  if (name && specialization) {
-    petShop.addVet(name, specialization);
+  const validationMessageElement =
+    document.getElementById("locationValidation");
+  const suggestionsList = document.getElementById("locationSuggestion");
+
+  if (name && specialization && location) {
+    petShop.addVet(name, specialization, location, (result) => {
+      if (result.success) {
+        // add validation message in vet section
+        validationMessageElement.textContent = `Vet has been successfully added in ${result.validatedLocation}`;
+        validationMessageElement.classList.add("text-green-500");
+
+        // cleanup input(s)
+        // .value = ""
+
+        setTimeout(
+          () => validationMessageElement.classList.add("hidden"),
+          2500
+        );
+      } else {
+        // add invalidation message in vet section
+        validationMessageElement.textContent = result.message;
+        validationMessageElement.classList.add("text-red-500");
+      }
+    });
   }
 
   updateVetsTable();
@@ -72,7 +113,7 @@ function updateVetsTable() {
   vetsTable.innerHTML = petShop.vets
     .map(
       (currentVet) =>
-        `<tr><td>${currentVet.name}</td><td>${currentVet.specialization}</td></tr>`
+        `<tr><td>${currentVet.name}</td><td>${currentVet.specialization}</td><td>${currentVet.location}</td></tr>`
     )
     .join("");
 }
@@ -98,3 +139,59 @@ function updateSelectOptions() {
 document
   .querySelector("#addVetButton")
   .addEventListener("click", () => addVet());
+
+// -----------------
+// Fetch & validate data
+// https://api.geonames.org/searchJSON?q=london&maxRows=2&username=adelin2202
+// -----------------
+function validateLocation(location, callback) {
+  const baseURL = "http://api.geonames.org";
+  const pathToSearch = "searchJSON";
+  const username = "adelin2202";
+
+  fetch(
+    `${baseURL}/${pathToSearch}?q=${location}&maxRows=5&username=${username}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const suggestionsList = document.getElementById("locationSuggestion");
+      suggestionsList.innerHTML = "";
+
+      const dataHasResults = data.geonames.length > 0;
+      if (data.geonames && dataHasResults) {
+        data.geonames.forEach((place) => {
+          const placeDetails = `${place.name}, ${place.countryName}`;
+
+          const li = document.createElement("li");
+
+          li.textContent = placeDetails;
+          li.classList.add("p-2", "hover:bg-zinc-500", "cursor-point");
+
+          li.addEventListener("click", () => {
+            // add value to input - what was clicked
+            document.getElementById("vetLocation").value = placeDetails;
+            suggestionsList.classList.add("hidden");
+
+            callback({
+              validatedLocation: placeDetails,
+              message: "Location is valid",
+            });
+          });
+
+          suggestionsList.appendChild(li);
+          suggestionsList.classList.remove("hidden");
+        });
+      } else {
+        suggestionsList.innerHTML = `<li class="p-2 hover:bg-zinc-500 cursor-point">No results</li>`;
+        suggestionsList.classList.remove("hidden");
+
+        setTimeout(() => {
+          suggestionsList.classList.add("hidden");
+          callback({
+            validatedLocation: null,
+            message: "No results or invalid location",
+          });
+        }, 2500);
+      }
+    });
+}
