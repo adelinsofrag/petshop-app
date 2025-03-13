e7ccdf3d855bd651a1f0e05777e58953df14a655
adelinsofrag <adelin.sofrag@gmail.com>
2025-03-13 19:28:55 +0200

    structure changes

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document
-  .querySelector("#addVetButton")
-  .addEventListener("click", handleAddVetButtonClick);
-
-document
-  .getElementById("vetLocation")
-  .addEventListener("input", (event) =>
-    handleOnInputChange(event.target.value)
-  );
